bfd5a0ab83ee29c456347e5b3fceb2b0b03e5e86
Mark Langhamer <langhamerm@gmail.com>
2020-04-28 16:58:56 -0500

    33

--- fbfs/src/components/projects/ProjectDetails.js
+++ fbfs/src/components/projects/ProjectDetails.js
@@ -1,25 +1,25 @@
+  
 import React from 'react'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
-import { Redirect } from "react-router-dom";
-
-
+import { Redirect } from 'react-router-dom'
+import moment from 'moment'
 
 const ProjectDetails = (props) => {
   const { project, auth } = props;
-    if (!auth.uid) return <Redirect to="/signin" />;
+  if (!auth.uid) return <Redirect to='/signin' /> 
   if (project) {
     return (
       <div className="container section project-details">
         <div className="card z-depth-0">
           <div className="card-content">
             <span className="card-title">{project.title}</span>
-            <p>{project.content}</p>
+            <p>{project.content}</p>s
           </div>
           <div className="card-action grey lighten-4 grey-text">
             <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-            <div>2nd September, 2am</div>
+            <div>{moment(project.createdAt.toDate()).calendar()}</div>
           </div>
         </div>
       </div>
@@ -41,7 +41,6 @@ const mapStateToProps = (state, ownProps) => {
   return {
     project: project,
     auth: state.firebase.auth
-
   }
 }
 
