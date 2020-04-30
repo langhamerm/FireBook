30687c37a80f6c4df26e408e04f15a8032791c81
Mark Langhamer <langhamerm@gmail.com>
2020-04-29 23:56:56 -0500

    d

--- fbfs/functions/index.js
+++ fbfs/functions/index.js
@@ -1,6 +1,5 @@
-const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const createNotification = ((notification) => {
