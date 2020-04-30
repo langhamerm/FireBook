d4b26597b65b722e064647256c1a0905f9247496
Mark Langhamer <langhamerm@gmail.com>
2020-04-29 22:39:14 -0500

    38

--- fbfs/functions/index.js
+++ fbfs/functions/index.js
@@ -1,5 +1,6 @@
-const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
 admin.initializeApp(functions.config().firebase);
 
 const createNotification = ((notification) => {
--- fbfs/src/components/dashboard/Dashboard.js
+++ fbfs/src/components/dashboard/Dashboard.js
@@ -5,19 +5,13 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { Redirect } from 'react-router-dom'
-// import styles from '../index.less';
-// import { b as bb } from '../../b';
-// import c from '../../c';
-// import a from 'a';
-// import React from 'react';
+
+
+
 class Dashboard extends Component {
   render() {
-    
-    // console.log(this.props);
-    const { projects, auth } = this.props;
-    
-    if (!auth.uid) return <Redirect to='/signin' />
-  
+    const { projects, auth, notifications } = this.props;
+    if (!auth.uid) return <Redirect to='/signin' /> 
 
     return (
       <div className="dashboard container">
@@ -26,7 +20,7 @@ class Dashboard extends Component {
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notifications />
+            <Notifications notifications={notifications} />
           </div>
         </div>
       </div>
@@ -34,18 +28,19 @@ class Dashboard extends Component {
   }
 }
 
-
 const mapStateToProps = (state) => {
   // console.log(state);
   return {
     projects: state.firestore.ordered.projects,
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications
   }
 }
 
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: 'projects' }
+    { collection: 'projects' },
+    { collection: 'notifications', limit: 3}
   ])
 )(Dashboard)
